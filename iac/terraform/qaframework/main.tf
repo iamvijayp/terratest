cdaa190395fd8c9665ed5a1a236ca2d577caab24
Vijay P <vijay.prahladan@gmail.com>
2023-01-09 13:01:07 +0530

    added main.tf

--- iac/terraform/qaframework/main.tf
+++ iac/terraform/qaframework/main.tf
@@ -1,3 +1,44 @@
 provider "aws" {
+  region = "us-east-2"
+}
+
+variable "bucket_name" {
+  type = string
+  description = "enter the bucket name"
+}
+## s3 bucket creation module
+
+module "s3bucket" {
+  source      = "./modules/s3bucket"
+  bucket_name = var.bucket_name
+}
+
+output "s3-bucketname" {
+  value = module.s3bucket.s3-bucketname
+}
+
+module "glue" {
+  source            = "./modules/glue-spark"
+  glue_s3_bucket    = module.s3bucket.s3-bucketname
+  glue_script_s3key = "scripts/glue-compress.py"
+  glue_script_path  = "glue-compress.py"
+
+  glue_job_name = "my glue job jenkins"
+  # role_arn = 
+
+  depends_on = [
+    module.s3bucket
+  ]
+}
+output "glue-job-name" {
+  value=module.glue.glue-job-name
+}
+
+output "glue-arn" {
+  value = module.glue.glue-job-arn
   
+}
+
+output "glue-job-id" {
+  value=module.glue.glue-job-id
 }
