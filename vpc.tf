40c0a0c2c4d64cc49fa74314d7a90f13c7359952
Vijay <iamvijayp@hotmail.com>
2024-01-02 18:10:19 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,76 @@
+# Specify the provider and region
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.6.0.0/16"
+}
+
+# Create private subnets
+resource "aws_subnet" "private" {
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(["10.6.0.0/20", "10.6.16.0/20", "10.6.32.0/20"], count.index)
+  map_public_ip_on_launch = false
+}
+
+# Create public subnets
+resource "aws_subnet" "public" {
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(["10.6.48.0/20", "10.6.64.0/20", "10.6.80.0/20"], count.index)
+  map_public_ip_on_launch = true
+}
+
+# Create an Internet Gateway and attach it to the VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create a public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Associate the public route table with the public subnets
+resource "aws_route_table_association" "public" {
+  count          = 3
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+# Create an EIP for the NAT gateway
+resource "aws_eip" "nat" {
+  vpc   = true
+}
+
+# Create a NAT gateway in the first public subnet
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+# Create private route tables
+resource "aws_route_table" "private" {
+  count  = 3
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+# Associate the private route tables with the private subnets
+resource "aws_route_table_association" "private" {
+  count          = 3
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
